dc2414f1373b73d8da01322ee78a80a9bae18078
joaopedrosf <joaopedrosf1903@gmail.com>
2023-12-14 08:53:26 -0300

    Adiciona autenticacao, validacoes e exceptions

--- backend/Models/DTOs/InsertUsuarioDTO.cs
+++ backend/Models/DTOs/InsertUsuarioDTO.cs
@@ -1,5 +1,7 @@
-﻿namespace vacinacao_backend.Models {
-    public class InsertUsuarioDTO {
+﻿namespace vacinacao_backend.Models.DTOs
+{
+    public record InsertUsuarioDTO
+    {
         public int Id { get; set; }
         public string Nome { get; set; }
         public DateOnly DataNascimento { get; set; }
@@ -9,6 +11,9 @@
         public string Setor { get; set; }
         public string Cidade { get; set; }
         public string UF { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
         public List<int>? Alergias { get; set; }
     }
 }
