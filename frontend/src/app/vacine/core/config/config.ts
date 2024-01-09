b59b387562dfddb3b28a264d51ade49843368fa2
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2024-01-09 19:34:55 -0300

    Update config.ts

--- frontend/src/app/vacine/core/config/config.ts
+++ frontend/src/app/vacine/core/config/config.ts
@@ -1,5 +1,5 @@
 export class Config{
-  readonly API_URL : string = 'https://api-vacinacao.onrender.com';
+  readonly API_URL : string = 'https://vacinacao-ds.onrender.com';
 
   static getApiUrl(): string {
     return new Config().API_URL;
