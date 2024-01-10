2c15f9bfdc9eaba81fedca753f1647eb9b34db1a
Veloso <alvaro.lisboa@docnix.com.br>
2024-01-10 02:49:39 -0300

    Correção User.ts

--- frontend/src/app/vacine/core/entities/User.ts
+++ frontend/src/app/vacine/core/entities/User.ts
@@ -1,23 +1,38 @@
-import Login from "./login";
+import Agenda from "./agenda";
+import Allergy from "./Allergy";
 
-export default class User{
-  constructor(name: string, rg: string = '', phone: string = '', birthDate: Date = new Date(), gender: string = '', motherName: string = '', cpf: string = '', login: Login = new Login('', '')) {
-    this.name = name;
-    this.rg = rg;
-    this.phone = phone;
-    this.birthDate = birthDate;
-    this.gender = gender;
-    this.motherName = motherName;
-    this.cpf = cpf;
-    this.login = login;
+export default class User {
+
+  id !: number;
+  nome !: string;
+  dataNascimento !: Date;
+  sexo !: string;
+  logradouro !: string;
+  numero !: number;
+  setor !: string;
+  cidade !: string;
+  uf !: string;
+  isAdmin !: boolean;
+  email !: string;
+  agendamentos !: Agenda[];
+  alergias !: Allergy[];
+
+
+  constructor(id: number, nome: string, dataNascimento: Date, sexo: string, logradouro: string, numero: number, setor: string, cidade: string, uf: string, isAdmin: boolean, email: string, agendamentos: Agenda[], alergias: Allergy[]) {
+    this.id = id;
+    this.nome = nome;
+    this.dataNascimento = dataNascimento;
+    this.sexo = sexo;
+    this.logradouro = logradouro;
+    this.numero = numero;
+    this.setor = setor;
+    this.cidade = cidade;
+    this.uf = uf;
+    this.isAdmin = isAdmin;
+    this.email = email;
+    this.agendamentos = agendamentos;
+    this.alergias = alergias;
   }
-
-  name!: string;
-  rg!: string;
-  phone!: string;
-  birthDate!: Date;
-  gender!: string;
-  motherName!: string;
-  cpf!: string;
-  login!: Login;
 }
+
+
