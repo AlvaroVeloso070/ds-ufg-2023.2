c97fd58d63654eff2bde42b7527f449f9c874005
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2024-01-22 21:34:53 -0300

    trocando o servidor de hospedagem do backend

--- frontend/src/app/vacine/core/config/config.ts
+++ frontend/src/app/vacine/core/config/config.ts
@@ -1,5 +1,5 @@
 export class Config{
-  readonly API_URL : string = 'https://vacinacao-ds.onrender.com';
+  readonly API_URL : string = 'http://161.35.235.79:8080';
 
   static getApiUrl(): string {
     return new Config().API_URL;
