8eb7ffdba4fc7a54a420d0956b467ac29c8acb13
Lucas Bernardes <lucasbernardes24@gmail.com>
2024-02-02 16:54:53 -0300

    provax-front api url

--- frontend/src/app/vacine/core/config/config.ts
+++ frontend/src/app/vacine/core/config/config.ts
@@ -1,5 +1,5 @@
 export class Config{
-  readonly API_URL : string = 'http://localhost:8080';
+  readonly API_URL : string = 'http://64.23.144.76:8080';
 
   static getApiUrl(): string {
     return new Config().API_URL;
